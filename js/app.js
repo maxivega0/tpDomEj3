5ae822b0752807eb1d0897b50a139b5ac5ad41ad
maxivega0 <maximilianovega600@gmail.com>
2023-04-15 17:44:38 -0300

    agregado boton borrar lista

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ mostrar.addEventListener("click", mostrarLista);
 let seccionPadre = document.querySelector("#contenedorPadre");
 
 let tareas = [];
-let bandera = false;
 
 function cargarTarea(e) {
     e.preventDefault();
@@ -18,32 +17,26 @@ function cargarTarea(e) {
 
 function mostrarLista() {
     let lista = document.getElementById("lista");
-    lista.innerHTML = ""; // Limpiamos la lista antes de volver a mostrarla
-
+    lista.innerHTML = "";
     if (tareas.length === 0) {
-        lista.innerHTML = `<div class="alert alert-danger my-3"><i class="bi bi-exclamation-circle-fill"></i> No hay tareas cargadas.</div>`;
-        bandera = true;
+        let mensaje = document.createElement("div");
+        mensaje.className = "alert alert-danger my-3";
+        mensaje.innerHTML = `<i class="bi bi-exclamation-circle-fill"></i> No hay tareas cargadas.`;
+        lista.appendChild(mensaje);
     } else if (tareas.length > 0) {
-        if (bandera === true) {
-            lista.removeChild(lista.children[0]);
-            bandera = false;
-        }
         for (let i = 0; i < tareas.length; i++) {
             let li = document.createElement("li");
-            li.textContent = tareas[i];
+            li.innerHTML = tareas[i];
             let botonEliminar = document.createElement("div");
-            botonEliminar.innerText = "Eliminar";
-            botonEliminar.className += "mx-3 btn btn-outline-danger"; // Agregamos una clase al botón para estilizarlo
-            botonEliminar.addEventListener("click", function() {
-                // Añadimos un evento de click al botón para eliminar la tarea
-                eliminarTarea(i);
-            });
-            li.appendChild(botonEliminar); // Añadimos el botón al elemento de lista
-            lista.appendChild(li); // Añadimos el elemento de lista a la lista
+            botonEliminar.innerHTML = "Eliminar";
+            botonEliminar.className += "mx-3 btn btn-outline-danger";
+            botonEliminar.addEventListener("click",()=>eliminarTarea(i));
+            li.appendChild(botonEliminar);
+            lista.appendChild(li);
         }
     }
 }
 function eliminarTarea(posicion) {
-    tareas.splice(posicion, 1); // Eliminamos la tarea del arreglo en la posición indicada
-    mostrarLista(); // Actualizamos la lista de tareas en la página
+    tareas.splice(posicion, 1); 
+    mostrarLista();
 }
